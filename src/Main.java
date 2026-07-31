490ec39086f92f62a5f8a049ae5d405265b160a1
mkrechkivskyi <misha.krechkivskyi25@gmail.com>
2026-07-31 21:56:56 +0300

    tests

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ public class Main {
 
     public static void main(String[] args) {
      stackFullOut(new Random().nextInt(), new Random().nextInt(), new Random().nextInt(),new Random().nextInt(),new Random().nextInt(),new Random().nextInt(),new Random().nextInt(),new Random().nextInt(),new Random().nextInt());
+        System.out.println("kdkdkdk");
     }
 
     static void heapFullOut(List<StringBuilder> builders){
