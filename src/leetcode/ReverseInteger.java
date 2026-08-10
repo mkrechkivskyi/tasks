a78f92d0e52abd400752789229dc96431c0ac5ec
mkrechkivskyi <misha.krechkivskyi25@gmail.com>
2026-08-10 12:25:09 +0300

    solved Reverse Integer task

---
+++ src/leetcode/ReverseInteger.java
@@ -0,0 +1,27 @@
+package leetcode;
+
+public class ReverseInteger {
+
+    public static void main(String[] args) {
+        int i = 432;
+        int reverse = new ReverseInteger().reverse(123);
+        System.out.println(reverse);
+    }
+
+    public int reverse(int x) {
+        long reversed = 0;
+
+        while (x != 0) {
+            reversed = reversed * 10 + (x % 10);
+            x /= 10;
+        }
+
+        // Проверяем на переполнение 32-битного int
+        if (reversed > Integer.MAX_VALUE || reversed < Integer.MIN_VALUE) {
+            return 0;
+        }
+
+        return (int) reversed;
+    }
+
+}
